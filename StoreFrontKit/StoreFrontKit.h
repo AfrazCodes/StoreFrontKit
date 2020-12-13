24bb6a6338fafb2457a30d58bf7b2ffac15c2f04
Afraz Siddiqui <afsiddiq@microsoft.com>
2020-12-13 11:36:53 -0500

    Initial Commit

---
+++ StoreFrontKit/StoreFrontKit.h
@@ -0,0 +1,18 @@
+//
+//  StoreFrontKit.h
+//  StoreFrontKit
+//
+//  Created by Afraz Siddiqui on 12/13/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StoreFrontKit.
+FOUNDATION_EXPORT double StoreFrontKitVersionNumber;
+
+//! Project version string for StoreFrontKit.
+FOUNDATION_EXPORT const unsigned char StoreFrontKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StoreFrontKit/PublicHeader.h>
+
+
---
+++ StoreFrontKitTests/StoreFrontKitTests.swift
@@ -0,0 +1,33 @@
+//
+//  StoreFrontKitTests.swift
+//  StoreFrontKitTests
+//
+//  Created by Afraz Siddiqui on 12/13/20.
+//
+
+import XCTest
+@testable import StoreFrontKit
+
+class StoreFrontKitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
